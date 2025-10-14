34d43c8ef0bad1329c5381736e8f35f12e55d67d
qwe <h@gmail.com>
2025-10-14 14:29:59 +0300

    Klases Velosipeds izstrādes process

---
+++ src/nhk/Velosipeds.java
@@ -0,0 +1,60 @@
+package nhk;
+
+import java.util.Random;
+
+public class Velosipeds {
+	// atr
+	private int ritenaD, sedeklPoz;
+	private double atrumsMS, cena;
+	private String razotajs;
+	
+	// konstr
+	public Velosipeds(int ritenaD, int sedeklPoz, double cena, String razotajs) {
+		
+		this.ritenaD = ritenaD;
+		this.sedeklPoz = sedeklPoz;
+		atrumsMS = kustiba();
+		this.cena = cena;
+		this.razotajs = razotajs;
+			
+	}
+	
+	// metodes
+	public int noteiktRitenaD() {
+		
+		return ritenaD;
+	}
+	public int noteiktSedeklPoz() {
+		
+		return sedeklPoz;
+	}
+	public String noteiktRaz() {
+		
+		return razotajs;
+	}
+	public double noteiktCenu() {
+		
+		return cena;
+	}
+	
+	public double kustiba() {
+		double celsM, laiksS; 
+		Random rand = new Random();
+		celsM = rand.nextDouble()*(1000-1)+1;
+		laiksS = rand.nextDouble()*(60-10)+10;
+		return Math.round(celsM/laiksS);
+	}
+	public double noteiktAtr() {
+		
+		return atrumsMS;
+	}
+	
+	public String izvadit() {
+		return "Riteņa ražotājs ~ "+noteiktRaz()+"\nRiteņa cena ~ "+noteiktCenu()+" EUR\nRiteņa lielums collās ~ "+noteiktRitenaD()
+		+"\nIestatītais sēdekļa augstums ~ "+noteiktSedeklPoz()+"\nĀtrums ~ "+noteiktAtr()+" m/s";
+		
+	}
+	
+	
+	
+}
